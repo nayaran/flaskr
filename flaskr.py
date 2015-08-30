0ee2147dec1e97536ee95f18e2ac4dab31931c88
narayan <anuragallahabad@gmail.com>
2015-08-30 10:29:03 +0530

    adds docstrings

--- flaskr.py
+++ flaskr.py
@@ -17,9 +17,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
+      """
+      Connects to the DATABASE db
+      """
       return sqlite3.connect(app.config['DATABASE'])
 
 def init_db():
+      """
+      Initializes the db -> creates the table
+      """
       with closing(connect_db()) as db:
             with app.open_resource('schema.sql', mode='r') as f:
                   db.cursor().executescript(f.read())
@@ -27,16 +33,27 @@ def init_db():
 
 @app.before_request
 def before_request():
+      """
+      Connect to the DB before aneveryy request
+      """
       g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
+      """
+      Disconnect after every request
+      """
       db = getattr(g, 'db', None)
       if db is not None:
             db.close()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+      """
+      Login View-
+      If method is GET- Returns the login page
+      If method is POST- Submits the login credentials
+      """
       error = None
       if request.method == 'POST':
             if request.form['username'] != app.config['USERNAME']:
@@ -51,12 +68,18 @@ def login():
 
 @app.route('/logout')
 def logout():
+      """
+      Logs out the currently logged in user
+      """
       session.pop('logged_in', None)
       flash('You were logged out')
       return redirect(url_for('show_entries'))
 
 @app.route('/')
 def show_entries():
+      """
+      Returns the view with all the entries from the db
+      """
       cur = g.db.execute('select title, text from entries order by id desc')
       entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
       return render_template('show_entries.html', entries=entries)
@@ -64,6 +87,9 @@ def show_entries():
 
 @app.route('/add', methods=['POST'])
 def add_entry():
+      """
+      Adds new entry in the db
+      """
       if not session.get('logged_in'):
             abort(401)
       g.db.execute('insert into entries (title, text) values (?, ?)',
@@ -73,4 +99,7 @@ def add_entry():
       return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
+      """
+      Runs the application on http://127.0.0.1:5000/
+      """
       app.run()
