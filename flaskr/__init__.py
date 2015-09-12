d45b16f3aa6f1525bfa28a28be01f8c1fc2877a1
narayan <anuragallahabad@gmail.com>
2015-09-13 01:23:38 +0530

    adds init file

---
+++ flaskr/__init__.py
@@ -0,0 +1,7 @@
+import flaskr
+import os
+
+# initialize the db for the first time
+if not os.path.isfile("/var/www/flaskr/flaskr/db/flaskr.db"):
+      print 'initialing the db...'
+      flaskr.init_db()
