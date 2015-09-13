9e44fc4edf081ebaba039f7032d9f474ecb6b625
narayan <anuragallahabad@gmail.com>
2015-09-13 16:32:18 +0530

    updates init file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,3 @@
 import flaskr
-import os
-
-# initialize the db for the first time
-if not os.path.isfile("/var/www/flaskr/flaskr/db/flaskr.db"):
-      print 'initialing the db...'
-      flaskr.init_db()
-
+from flaskr import app, init_db
 
